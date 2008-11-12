6f891d406749837b2c3ee4c2cae349bf5d72e707
Walter Doekes <walter+github@wjd.nu>
2008-11-12 14:28:01 +0000

    initial import

---
+++ pyl10n/pyl10n.py
@@ -0,0 +1,247 @@
+#!/usr/bin/env python
+# vim:ts=8:sw=4:sts=4:et
+import __builtin__, os, cPickle as pickle, sys
+
+#
+# IMPLEMENTED:
+#
+# Most functions behave very or exactly similarly to their locale.* counterpart.
+#
+# Globals modification:
+# * setlocale() <= application wide locale setting: use nl_NL instead of nl_NL.utf-8!
+# * setlocalefunc() <= uses a callable should return a string (for django this would be
+#   lambda: to_locale(get_language()))
+#
+# The following functions take an optional callable so you don't have to call
+# setlocalefunc():
+# * localeconv() <= works as intended
+# * localeconvext() <= loads all available locale categories into a dictionary
+# * format() <= limited to a single format specified (e.g. '%08d')
+# * currency() <= works as intended
+# * str() <= calls format with %f
+
+#
+# NOT IMPLEMENTED:
+#
+# * Must be implemented: atof, atoi, 
+# * Will be replaced: DAY_1 etc.. will be replaced with a time formatting function.
+# * Should be implemented: address/telephone formatting functions
+# * Won't be implemented any time soon: strcoll, strxfrm, format_string.
+#
+
+#
+# BUGS:
+#
+# * Exception handling in _get_category is not properly done yet.
+#
+# * The locale_path is 'hardcoded' as relative to __file__.
+#
+# * The standard is unclear about how to handle [np]_sep_by_space for international
+#   symbols. We assume that the space is an optional fourth character in int_curr_symbol.
+#
+# * format(...monetary=True) gets handled by currency(). This means that your format string
+#   will be ignored.
+#
+# * string.letters is not modified when calling setlocale(). (In fact, no CTYPE/COLLATE
+#   locale settings are used.)
+#
+
+#
+# PYTHON BUGS:
+#
+# * Python is inconsistent with stripping or not stripping trailing zeroes from lists.
+#
+# * Python does not honor mon_grouping for monetary values.
+#
+
+_locale_path = os.path.join(os.path.dirname(__file__), 'locale')
+_current_lang_callable = lambda: 'C'
+
+def setlocale(language):
+    setlocalefunc(lambda: language)
+
+def setlocalefunc(callable):
+    global _current_lang_callable
+    _current_lang_callable = callable
+
+def localeconv(callable=None):
+    language = _get_language(callable)
+    ret = {}
+    ret.update(_get_category(language, 'LC_MONETARY'))
+    ret.update(_get_category(language, 'LC_NUMERIC'))
+    # We've removed trailing zeroes at generation time
+    assert 0 not in ret['grouping'] and 0 not in ret['mon_grouping']
+    return ret
+
+def localeconvext(callable=None):
+    language = _get_language(callable)
+    ret = {}
+    for category in ('LC_ADDRESS', 'LC_MEASUREMENT', 'LC_MONETARY', 'LC_NAME', \
+            'LC_NUMERIC', 'LC_PAPER', 'LC_TELEPHONE', 'LC_TIME'):
+        ret[category] = _get_category(language, category)
+    return ret
+
+def format(format, val, grouping=False, monetary=False, callable=None):
+    if monetary:
+        assert format == u'%f'
+        return currency(val, False, grouping, callable=callable)
+        
+    assert len(format) and format[0] == u'%'
+    conv = localeconv(callable)
+
+    ret = format % val
+    if u'e' in ret: # we're looking at exponents.. blame the user
+        return ret
+    return _group_and_decimal(ret, grouping, conv['decimal_point'], \
+            conv['thousands_sep'], conv['grouping'])
+
+def currency(val, symbol=True, grouping=False, international=False, callable=None):
+    conv = localeconv(callable)
+    neg = val < 0
+    val = abs(val)
+
+    if neg:
+        symbol_before = bool(conv['n_cs_precedes'])
+        sign = conv['negative_sign']
+        sep_by_space = bool(conv['n_sep_by_space'])
+        positioning = conv['n_sign_posn']
+    else:
+        symbol_before = bool(conv['p_cs_precedes'])
+        sign = conv['positive_sign']
+        sep_by_space = bool(conv['p_sep_by_space'])
+        positioning = conv['p_sign_posn']
+
+    if international:
+        symbol_char = conv['int_curr_symbol']
+        fractionals = int(conv['int_frac_digits'])
+        if len(symbol_char) > 3:
+            space_between_symbol_value = symbol_char[3]
+        else:
+            space_between_symbol_value = ''
+        symbol_char = symbol_char[0:3]
+    else:
+        symbol_char = conv['currency_symbol']
+        fractionals = int(conv['frac_digits'])
+        space_between_symbol_value = ('', ' ')[sep_by_space]
+
+    ret = (u'%%.%if' % fractionals) % val
+    ret = _group_and_decimal(ret, grouping, conv['mon_decimal_point'], \
+            conv['mon_thousands_sep'], conv['mon_grouping'])
+
+    if not symbol:
+        if positioning == 0:
+            return u'(%s%s%s)' % (ret,)
+        elif positioning == 1 or positioning == 3 or positioning == 127:
+            return u'%s%s' % (sign, ret)
+        elif positioning == 2 or positioning == 4:
+            return u'%s%s' % (ret, sign)
+        assert False
+        
+    if symbol_before:
+        args = [symbol_char, space_between_symbol_value, ret]
+    else:
+        args = [ret, space_between_symbol_value, symbol_char]
+
+    if positioning == 0:
+        ret = u'(%s%s%s)' % tuple(args)
+    else:
+        if positioning == 1 or (positioning == 3 and symbol_before) or positioning == 127:
+            args.insert(0, sign)
+        elif positioning == 2 or (positioning == 4 and not symbol_before):
+            args.append(sign)
+        elif positioning == 3:
+            args.insert(2, sign)
+        elif positioning == 4:
+            args.insert(1, sign)
+        else:
+            assert False
+        ret = u'%s%s%s%s' % tuple(args)
+
+    return ret
+
+def str(val, callable=None):
+    return format('%f', val, callable=callable)
+
+def _get_category(language, category):
+    try:
+        data = open(os.path.join(_locale_path, language, category), 'rb')
+        ret = pickle.load(data)
+        assert type(ret) == dict
+        return ret
+    except Exception, e:
+        print type(e), e
+        return {}
+
+def _get_language(callable = None):
+    global _current_lang_callable
+    callable = callable or _current_lang_callable
+    return _current_lang_callable()
+
+def _group(val, group_char, group_list):
+    if val[0] not in '0123456789':
+        sign = val[0]
+        val = val[1:]
+    else:
+        sign = ''
+
+    ret = []
+    i = len(val)
+    # group_list defines grouping from right to left
+    group = 127
+    for group in group_list:
+        if i <= 0 or group == 127: # CHAR_MAX => no more grouping
+            break
+        # append next group to list
+        ret.insert(0, val[max(0,i-group):i])
+        i -= group
+    # continue with last value from group_list
+    while i > 0:
+        ret.insert(0, val[max(0,i-group):i])
+        i -= group
+    # concat and return
+    return sign + group_char.join(ret)
+    
+def _group_and_decimal(val, grouping, decimal_char, group_char, group_list):
+    if grouping:
+        if u'.' in val:
+            left, right = val.split(u'.')
+            return u'%s%s%s' % (_group(left, group_char, group_list), decimal_char, right)
+        else:
+            return _group(val, group_char, group_list)
+    return val.replace(u'.', decimal_char)
+
+if __name__ == '__main__':
+    import locale
+    for lang in ('nl_NL', 'en_US'):
+        print u'**** %s ****\n' % (lang,)
+        locale.setlocale(locale.LC_MONETARY, lang + '.utf-8')
+        locale.setlocale(locale.LC_NUMERIC, lang + '.utf-8')
+        setlocale(lang)
+
+        print u'%24s|%24s' % ('[locale]', '[pyl10n]')
+        lconv = locale.localeconv()
+        pconv = localeconv()
+        keys = lconv.keys()
+        keys.sort()
+        for k in keys:
+            print u'%24s|%24s <= %s' % (__builtin__.str(lconv[k]).decode('utf-8'), pconv[k], k)
+        print
+
+        print '%24s|%24s' % ('[locale]', '[pyl10n]')
+
+        print '%24s|%24s' % (locale.str(-3.1415), str(-3.1415))
+        for val in (0.7, -0.7, 1234567.89, -1234567.89):
+            for monetary in (False, True):
+                lval = locale.format('%f', val, True, monetary).decode('utf-8')
+                pval = format('%f', val, True, monetary)
+                print '%24s|%24s' % (lval, pval)
+        for val in (0.7, -0.7, 1234567.89, -1234567.89):
+            for intl in (False, True):
+                lval = locale.currency(val, True, True, intl).decode('utf-8')
+                pval = currency(val, True, True, intl)
+                print '%24s|%24s' % (lval, pval)
+        print
+
+        print 'All available locale data:\n  ', localeconvext()
+        print
+
---
+++ pyl10n/pyl10ngen.py
@@ -0,0 +1,218 @@
+#!/usr/bin/env python
+# vim:ts=8:sw=4:sts=4:et
+import itertools, os, re
+
+#
+# BUGS:
+#
+# * Comment might be legal further on the line, but its not used often (only 3 out of 226 files do that).
+#   E.g.: Language file as_IN corrupt: unexpected data: IGNORE;IGNORE;IGNORE;%... (%...)
+#
+# * We only use the CATEGORIES defined below, and not LC_CTYPE/LC_COLLATE.. they are complicated and beyond the scope
+#   of the fix, right now.
+#
+# * I suspect that the copy keyword in a category tells us that you should only read that category from the copied
+#   file. Currently it reads everything from copied files (and uses loop_check to fix infinite recursion).
+#
+# * There is an 'include' keyword. We don't use it yet.
+#
+# * We strip trailing zeroes from a list (e.g. mon_grouping 3;3;0 becomes [3,3]). This is not a bug but a deviation
+#   from the C-documentation.
+#
+
+
+# Read only these categories.. set to None to read all.
+CATEGORIES = ('LC_ADDRESS', 'LC_MEASUREMENT', 'LC_MONETARY', 'LC_NAME', 'LC_NUMERIC', 'LC_PAPER', 'LC_TELEPHONE', 'LC_TIME')
+
+class ParseException(Exception):
+    pass
+
+def parse_args(string, escape_char):
+    re_nextarg = re.compile(r'(\s*(LC_[A-Z]+|[A-Za-z_:-]+|"[^"]*"|-?[0-9]+|<[0-9A-Za-z_-]+>|<[0-9A-Za-z_-]+>..<[0-9A-Za-z_-]+>|\(<[0-9A-Za-z_-]+>,<[0-9A-Za-z_-]+>\))\s*)')
+    ret = []
+
+    i = 0
+    while True:
+        m = re_nextarg.match(string, i)
+        if not m:
+            raise ParseException, 'Unexpected data: \'\'\'%s<-- HERE -->%s\'\'\'' % (string[:i], string[i:])
+        ret.append(m.group(2))
+        i = m.end(1)
+        if i < len(string) and string[i] == ';':
+            i += 1
+            continue
+        break
+
+    for i in range(len(ret)):
+        if ret[i].isdigit() or ret[i][0] == '-' and ret[i][1:].isdigit():
+            ret[i] = int(ret[i])
+        elif ret[i][0] == '"':
+            try:
+                ret[i] = unicode(ret[i][1:-1].replace(escape_char * 2, escape_char))
+                j = 0
+                while j < len(ret[i]):
+                    if ret[i][j] == '<' and ret[i][j+1] == 'U' and ret[i][j+6] == '>':
+                        ret[i] = ret[i][0:j] + unichr(int(ret[i][j+2:j+6], 16)) + ret[i][j+7:]
+                    j += 1
+            except UnicodeDecodeError:
+                ret[i] = 'Someone put illegal non-ASCII here!'
+        # else: <U0123> or <U01234567>
+        # else: <U0123>..<U4567>
+        # else: <TREMA>
+
+    return ret
+    
+
+def parse_libc_localedata(filename, loop_check = None):
+    # Trap infinite recursion
+    loop_check = loop_check or []
+    if filename in loop_check:
+        return {}
+    loop_check.append(filename)
+
+    # The result
+    ret = {}
+
+    input = open(filename, 'rb')
+    comment_char, escape_char = '#', '\\'
+    category = None
+    state = None
+    multiline_buffer = False
+    
+    re_cat = re.compile(r'^(LC_[A-Z]+)$', re.DOTALL)
+    re_stmt = re.compile(r'^([A-Za-z_]+)\s+(.*?)$', re.DOTALL)
+    re_comment_s = r'^Comment.*$'
+    re_comment = re.compile(re_comment_s.replace('Comment', comment_char*(int(comment_char=='\\')+1)))
+    re_multiline_s = r'(^|[^Escape]|EscapeEscape)*Escape$'
+    re_multiline = re.compile(re_multiline_s.replace('Escape', escape_char*(int(escape_char=='\\')+1)))
+
+    for line in itertools.chain(input, ('\n',)): # append extra line to complete work
+        # Ignore extra whitespace
+        line = line.strip()
+        # Ignore comments (escape char on EOL does not "work")
+        if re_comment.match(line):
+            continue
+        # If previous line was multiline, append
+        if multiline_buffer != False:
+            line = multiline_buffer + line.strip()
+        # If line matches multiline, set multiline_buffer and restart
+        if re_multiline.match(line):
+            multiline_buffer = line[:-len(escape_char)].strip() # strip that one too..
+            continue
+        multiline_buffer = False
+
+        # Ignore empty lines
+        if line == '':
+            pass
+        # Check categories
+        elif re_cat.match(line):
+            m = re_cat.match(line)
+            category = m.group(1)
+            ret[category] = {}
+        # Check statements
+        elif re_stmt.match(line):
+            m = re_stmt.match(line)
+            keyword, args = m.group(1), m.group(2)
+            # Special case where we close a category
+            if keyword == 'END' and args == category: # e.g. "END LC_NUMERIC"
+                category = None
+            # Skip over categories that we're not interested in
+            elif category != None and (CATEGORIES != None and category not in CATEGORIES):
+                pass
+            # Switch comment character
+            elif keyword == 'comment_char':
+                comment_char = args
+                re_comment = re.compile(re_comment_s.replace('Comment', comment_char*(int(comment_char=='\\')+1)))
+            # Switch escape character
+            elif keyword == 'escape_char':
+                escape_char = args
+                re_multiline = re.compile(re_multiline_s.replace('Escape', escape_char*(int(escape_char=='\\')+1)))
+            # Load other file
+            elif keyword == 'copy':
+                recurse_data = parse_libc_localedata(
+                    os.path.join(os.path.dirname(filename), parse_args(args, escape_char)[0]),
+                    loop_check
+                )
+                # Merge inner file data
+                for recurse_cat in recurse_data:
+                    if recurse_cat not in ret:
+                        ret[recurse_cat] = {}
+                    ret[recurse_cat].update(recurse_data[recurse_cat])
+            # Wow.. we even have defines...
+            elif keyword == 'ifdef' or keyword == 'define':
+                pass
+            # Order start and end? Forget it...
+            elif keyword == 'order_start':
+                pass
+            # Args.. store them
+            else:
+                assert args != ''
+                args = parse_args(args, escape_char)
+                # One-sized lists are not lists
+                if len(args) == 1:
+                    args = args[0]
+                # Lists stay lists (even when empty)
+                else:
+                    # Trim tailing zeroes from lists (see locale.localeconv() output)
+                    while len(args) >= 1 and args[-1] == 0:
+                        args.pop()
+                ret[category][keyword] = args
+        # No match
+        else:
+            # Skip over uninteresting categories
+            if category != None and (CATEGORIES != None and category not in CATEGORIES):
+                pass
+            # We don't do defines
+            elif keyword == 'else' or keyword == 'endif':
+                pass
+            # And we don't do order start and end.
+            elif keyword == 'order_end':
+                pass
+            # We don't do anything with non-statements, really ;)
+            else:
+                pass
+                #print '(unmatched: %s)' % (line,)
+   
+    # Purge empty categories
+    for key in ret.keys():
+        if len(ret[key]) == 0:
+            del ret[key]
+    return ret
+
+
+if __name__ == '__main__':
+    import cPickle as pickle, re, os, sys
+    file_match = re.compile(r'^[a-z]+_[A-Z]+$')
+    locale_source = '/usr/share/i18n/locales'
+    locale_destination = os.path.join(os.path.dirname(__file__), 'locale')
+
+    in_languages = []
+    for file in os.listdir(locale_source):
+        if file_match.match(file):
+            in_languages.append(file)
+    in_languages.sort()
+
+#    in_languages = ['csb_PL']
+#
+#    print parse_args('"<U003B>"', '/')
+#    import sys
+#    sys.exit(1)
+    
+    print 'Processing languages...'
+    for language in in_languages:
+        sys.stdout.write('\r%s\r%s' % (' ' * 16, language))
+        sys.stdout.flush()
+        try:
+            locale_info = parse_libc_localedata(os.path.join(locale_source, language))
+            try: os.mkdir(os.path.join(locale_destination, language))
+            except OSError: pass
+
+            for category in locale_info:
+                destination = open(os.path.join(locale_destination, language, category), 'wb')
+                pickle.dump(locale_info[category], destination, pickle.HIGHEST_PROTOCOL)
+
+        except ParseException, e:
+            sys.stdout.write('\n')
+            sys.stderr.write('Language file %s corrupt: %s\n' % (language, e))
+
+    print '\r%s\r...done' % (' ' * 16,)
